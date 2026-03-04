6b108d11d4e12b3aa5f1f34e681b29cb1a95644f
chandrabumi26 <chandra.bumi@gmail.com>
2026-03-04 18:44:39 +0700

    get rid unecessary

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -88,22 +88,6 @@ export default function Hero() {
                         Get In Touch
                     </motion.a>
                 </motion.div>
-
-                {/* Scroll indicator */}
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 1.2 }}
-                    className="absolute bottom-10 left-1/2 -translate-x-1/2"
-                >
-                    <motion.div
-                        animate={{ y: [0, 10, 0] }}
-                        transition={{ duration: 1.5, repeat: Infinity }}
-                        className="w-6 h-10 rounded-full border-2 border-charcoal/30 flex items-start justify-center p-2"
-                    >
-                        <motion.div className="w-1.5 h-1.5 rounded-full bg-tosca" />
-                    </motion.div>
-                </motion.div>
             </div>
         </section>
     );
